de2d35cf8e27cbd77e7f65973df87b5b50342ba7
Daniel <daniel.m.adr@hotmail.com>
2024-04-16 10:03:14 -0300

    first commit

--- SkyFeedConnect/src/main/java/com/Ada/SkyFeedConnect/Application.java
+++ SkyFeedConnect/src/main/java/com/Ada/SkyFeedConnect/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkyFeedConnectApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkyFeedConnectApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
