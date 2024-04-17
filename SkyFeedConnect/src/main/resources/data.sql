1fa084b58e72474c9191ebe4b5344ffa519109af
Daniel <daniel.m.adr@hotmail.com>
2024-04-17 11:46:32 -0300

    feat: data base config

---
+++ SkyFeedConnect/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    login    VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
