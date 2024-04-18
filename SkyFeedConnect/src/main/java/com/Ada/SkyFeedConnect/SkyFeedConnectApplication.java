3b2f35bc4c05baa606ef54cc102990471203a9e6
PHFelipe <paulo.academico01@gmail.com>
2024-04-18 13:59:37 -0300

    first config

---
+++ SkyFeedConnect/src/main/java/com/Ada/SkyFeedConnect/SkyFeedConnectApplication.java
@@ -0,0 +1,13 @@
+package com.Ada.SkyFeedConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyFeedConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyFeedConnectApplication.class, args);
+	}
+
+}
