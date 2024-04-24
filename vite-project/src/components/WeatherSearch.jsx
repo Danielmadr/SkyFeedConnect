fbdeff8953af79119c55d6d19a70847f7d82c5de
Daniel <daniel.m.adr@hotmail.com>
2024-04-24 18:31:15 -0300

    formart: breakline in weather widget

--- vite-project/src/components/WeatherSearch.jsx
+++ vite-project/src/components/WeatherSearch.jsx
@@ -4,14 +4,16 @@ const WeatherWidget = () => {
   // Lógica para chamar a API do clima e renderizar os dados
   return (
     <div id="weather">
-      <h1>Condiçoes do Tempo</h1>
+      <h1>
+        Condiçoes <br /> do <br /> Tempo
+      </h1>
       <section id="search-form">
         <form>
           <input
             className="inputWidget"
             type="text"
             id="input-local"
-            placeholder="Insira a Localização..."
+            placeholder="Insira o local..."
           />
           <button id="button-local">Pesquisar</button>
         </form>
