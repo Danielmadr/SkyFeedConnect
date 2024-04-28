1d216251fb7505cef9243722f5622aba58309f41
Daniel <daniel.m.adr@hotmail.com>
2024-04-28 15:50:57 -0300

    fix: login sign page centralized

--- vite-project/src/pages/LoginPage/LoginPage.jsx
+++ vite-project/src/pages/LoginPage/LoginPage.jsx
@@ -39,50 +39,52 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="sign-in-container">
-      <h1 className="title-sign">Sign In</h1>
-      <p className="subtitle">Welcome to Sky Feed Connect!</p>
-      <form onSubmit={handleSignIn} className="sign-in-form">
-        <input
-          type="email"
-          placeholder="Email"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button
-          type="submit"
-          className="sign-in-button"
-          disabled={isSubmitting}
-        >
-          {isSubmitting ? "Signing In..." : "Sign In"}
-        </button>
-        <button
-          type="button"
-          onClick={handleForgotPassword}
-          className="forgot-password-link"
-        >
-          Forgot Password?
-        </button>
-      </form>
-      {error && (
-        <p className="error-message" style={{ color: "red" }}>
-          {error}
+    <div id="login-page">
+      <div className="sign-in-container">
+        <h1 className="title-sign">Sign In</h1>
+        <p className="subtitle">Welcome to Sky Feed Connect!</p>
+        <form onSubmit={handleSignIn} className="sign-in-form">
+          <input
+            type="email"
+            placeholder="Email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button
+            type="submit"
+            className="sign-in-button"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Signing In..." : "Sign In"}
+          </button>
+          <button
+            type="button"
+            onClick={handleForgotPassword}
+            className="forgot-password-link"
+          >
+            Forgot Password?
+          </button>
+        </form>
+        {error && (
+          <p className="error-message" style={{ color: "red" }}>
+            {error}
+          </p>
+        )}
+        <p className="sign-in-redirect">
+          Not a Member yet?{" "}
+          <span className="sign-up-link" onClick={handleSignUpClick}>
+            Sign up
+          </span>
         </p>
-      )}
-      <p className="sign-in-redirect">
-        Not a Member yet?{" "}
-        <span className="sign-up-link" onClick={handleSignUpClick}>
-          Sign up
-        </span>
-      </p>
+      </div>
     </div>
   );
 };
